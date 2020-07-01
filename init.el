02e470337e33d3f80caf46b8fdb6d71a68659550
jakosimov <jakobalwall@gmail.com>
2020-07-01 03:44:54 +0200

    Fixa lsp config.

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
@@ -12,7 +13,7 @@
     ("76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "a41b81af6336bd822137d4341f7e16495a49b06c180d6a6417bf9fd1001b6d2b" "28caf31770f88ffaac6363acfda5627019cac57ea252ceb2d41d98df6d87e240" "669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" "332fcf3c7208aca9fab65d54203f78a242482e7fd65f5725a2482c20b1730732" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "a7051d761a713aaf5b893c90eaba27463c791cd75d7257d3a8e66b0c8c346e77" default)))
  '(package-selected-packages
    (quote
-    (zenburn-theme doom-modeline rust-mode magit pretty-mode treemacs mood-line racket-mode lsp-haskell atom-one-dark-theme web-mode projectile company-lsp lsp-ui yasnippet typescript-mode dracula-theme org-bullets lsp-mode haskell-mode smartparens company flycheck ivy))))
+    (lsp-haskell company-lsp lsp-ui lsp-mode hasky-stack cmake-ide cmake-project rtags yaml-mode zenburn-theme doom-modeline rust-mode magit pretty-mode treemacs mood-line racket-mode atom-one-dark-theme web-mode projectile yasnippet typescript-mode dracula-theme org-bullets haskell-mode smartparens company flycheck ivy))))
 
 ;; Yoga:
 ;;   FF: Source Code Pro
@@ -27,6 +28,9 @@
 (require 'ace-window)
 (require 'magit)
 (load "~/.emacs.d/terminal-thing")
+
+(defvar enable-c++-lsp 1)
+(defvar enable-haskell-lsp 1)
 
 (defun customize-emacs ()
   "Opens the custom.el file."
@@ -53,8 +57,6 @@
   (add-hook 'c-mode-common-hook (lambda ()
                                   (electric-pair-mode 1)
                                   (smartparens-mode -1)))
-  (add-hook 'haskell-mode-hook (lambda ()
-                                 (flycheck-mode -1)))
   (add-hook 'org-mode-hook (lambda ()
                              (visual-line-mode 1)))
   (add-hook 'typescript-mode-hook #'lsp)
@@ -62,7 +64,13 @@
   (add-hook 'racket-mode-hook (lambda () (flycheck-mode -1)))
   (add-hook 'org-mode-hook 'org-bullets-mode)
   (add-hook 'emacs-lisp-mode-hook (lambda ()
-				    (local-set-key (kbd "C-c C-e") 'eval-buffer))))
+				    (local-set-key (kbd "C-c C-e") 'eval-buffer)))
+  (if enable-c++-lsp
+      (add-hook 'c++-mode-hook 'lsp))
+  (if enable-haskell-lsp
+      (add-hook 'haskell-mode-hook 'lsp))
+
+  )
 
 (defun set-global-keys ()
   (global-set-key (kbd "C-c j") 'toggle-terminal-horizontal)
@@ -74,7 +82,13 @@
   (global-set-key (kbd "C-x w") 'kill-ring-save)
   (global-set-key (kbd "C-x o") 'ace-window))
 
-
+(defun my-lsp-setup ()
+ (setq lsp-ui-doc-enable nil)
+ (setq lsp-enable-symbol-highlighting nil)
+ (add-hook 'lsp-mode-hook (lambda ()
+                            (local-set-key (kbd "C-c d") 'lsp-find-definition)
+                            (local-set-key (kbd "C-c h") 'lsp-ui-doc-show)
+                            (local-set-key (kbd "C-c r") 'lsp-ui-peek-find-references))))
 
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
@@ -92,5 +106,11 @@
 (initiate-modes)
 (initiate-hooks)
 (set-global-keys)
+(my-lsp-setup)
 
-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
