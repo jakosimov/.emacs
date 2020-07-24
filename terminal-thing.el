68141d112af017b38e4158ed77163b6c79679759
jakosimov <jakobalwall@gmail.com>
2020-07-24 04:49:16 +0200

    Fixa terminalen lite.

--- terminal-thing.el
+++ terminal-thing.el
@@ -20,15 +20,22 @@
 
 (defun find-string-index (target alist)
   "Does something with TARGET and ALIST."
-  (position-if (lambda (s) (string= s target)) alist))
+  (position-if (lambda (s) (string-prefix-p target s)) alist))
 
 (defun initialize-terminal-buffer ()
-  (vterm))
+  (interactive)
+  (vterm)
+  (if (not term-mode-line-enabled)
+      (setq mode-line-format nil)))
 
-(defun get-terminal-buffer ()
-  "Does something."
+(defun find-terminal-buffer-index ()
   (let* ((buffer-names (mapcar (function buffer-name) (buffer-list)))
          (term-index (find-string-index actual-term-name buffer-names)))
+    term-index))
+
+(defun get-a-terminal-buffer ()
+  "Does something."
+  (let* ((term-index (find-terminal-buffer-index)))
     (if term-index
         (switch-to-buffer (nth term-index (buffer-list)))
       (initialize-terminal-buffer))))
@@ -40,18 +47,24 @@
          (side (not is-horizontal)))
          (split-window (frame-root-window) size side)))
 
-(defun open-terminal (is-horizontal)
+(defun open-terminal-window (is-horizontal)
   (let ((new-window (create-new-window is-horizontal)))
     (select-window new-window)
-    (get-terminal-buffer)
-    (if (not term-mode-line-enabled)
-	(setq mode-line-format nil))))
+    (get-a-terminal-buffer)))
+
+(defun get-terminal-window ()
+  (let* ((window-names (mapcar (lambda (win) (buffer-name (window-buffer win)))
+                               (window-list)))
+         (window-index (find-string-index actual-term-name window-names)))
+    (if window-index
+        (nth window-index (window-list))
+      nil)))
 
 (defun toggle-terminal (is-horizontal)
-  (let ((term-window (get-buffer-window actual-term-name)))
+  (let ((term-window (get-terminal-window)))
     (if term-window
         (delete-window term-window)
-      (open-terminal is-horizontal))))
+      (open-terminal-window is-horizontal))))
 
 (defun toggle-terminal-horizontal ()
   "Toggle a small horizontal terminal window."
@@ -62,5 +75,27 @@
   "Toggle a small vertical terminal window."
   (interactive)
   (toggle-terminal nil))
+
+(defun cd-to-current ()
+  "CDs to the current buffer directory."
+  (interactive)
+  (let ((dir (expand-file-name default-directory))
+        (terminal-window (get-terminal-window)))
+    (if (not terminal-window)
+        (open-terminal-window t)
+      (select-window terminal-window))
+    (vterm-send-string (concat "cd " dir) t)
+    (vterm-send-return)
+    (vterm-clear)))
+
+(defun create-new-terminal ()
+  (interactive)
+  (let ((dir (expand-file-name default-directory))
+        (terminal-window (get-terminal-window)))
+    (if (not terminal-window)
+        (open-terminal-window t)
+      (select-window terminal-window))
+    (vterm--set-directory dir)
+    (initialize-terminal-buffer)))
 
 (provide 'terminal-thing)
