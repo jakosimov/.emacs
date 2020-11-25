9f43d6b01cd3a406ef457c5eda7e64e79a459bcc
Eruvarn <jakobalwall@gmail.com>
2020-11-25 18:35:33 +0100

    ??

--- init.el
+++ init.el
@@ -70,6 +70,97 @@
      `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
         1 'org-checkbox-done-text prepend))
      'append))
+  ;; LATEX---
+  (defvar org-latex-fragment-last nil
+    "Holds last fragment/environment you were on.")
+
+  (defun my/org-latex-fragment--get-current-latex-fragment ()
+    "Return the overlay associated with the image under point."
+    (car (--select (eq (overlay-get it 'org-overlay-type) 'org-latex-overlay) (overlays-at (point)))))
+
+  (defun my/org-in-latex-fragment-p ()
+    "Return the point where the latex fragment begins, if inside
+  a latex fragment. Else return false"
+    (let* ((el (org-element-context))
+           (el-type (car el)))
+      (and (or (eq 'latex-fragment el-type) (eq 'latex-environment el-type))
+           (org-element-property :begin el))))
+
+  (defun org-latex-fragment-toggle-auto ()
+    ;; Wait for the s
+    (interactive)
+    (while-no-input
+      (run-with-idle-timer 0.05 nil 'org-latex-fragment-toggle-helper)))
+
+  (defun org-latex-fragment-toggle-helper ()
+    "Toggle a latex fragment image "
+    (condition-case nil
+        (and (eq 'org-mode major-mode)
+             (let* ((begin (my/org-in-latex-fragment-p)))
+               (cond
+                ;; were on a fragment and now on a new fragment
+                ((and
+                  ;; fragment we were on
+                  org-latex-fragment-last
+                  ;; and are on a fragment now
+                  begin
+                  ;; but not on the last one this is a little tricky. as you edit the
+                  ;; fragment, it is not equal to the last one. We use the begin
+                  ;; property which is less likely to change for the comparison.
+                  (not (= begin
+                          org-latex-fragment-last)))
+                 ;; go back to last one and put image back
+                 (save-excursion
+                   (goto-char org-latex-fragment-last)
+                   (when (my/org-in-latex-fragment-p) (org-latex-preview))
+                   ;; now remove current imagea
+                   (goto-char begin)
+                   (let ((ov (my/org-latex-fragment--get-current-latex-fragment)))
+                     (when ov
+                       (delete-overlay ov)))
+                   ;; and save new fragment
+                   (setq org-latex-fragment-last begin)))
+
+                ;; were on a fragment and now are not on a fragment
+                ((and
+                  ;; not on a fragment now
+                  (not begin)
+                  ;; but we were on one
+                  org-latex-fragment-last)
+                 ;; put image back on
+                 (save-excursion
+                   (goto-char org-latex-fragment-last)
+                   (when (my/org-in-latex-fragment-p)(org-latex-preview)))
+
+                 ;; unset last fragment
+                 (setq org-latex-fragment-last nil))
+
+                ;; were not on a fragment, and now are
+                ((and
+                  ;; we were not one one
+                  (not org-latex-fragment-last)
+                  ;; but now we are
+                  begin)
+                 (save-excursion
+                   (goto-char begin)
+                   ;; remove image
+                   (let ((ov (my/org-latex-fragment--get-current-latex-fragment)))
+                     (when ov
+                       (delete-overlay ov)))
+                   (setq org-latex-fragment-last begin)))
+                ;; else not on a fragment
+                ((not begin)
+                 (setq org-latex-fragment-last nil)))))
+      (error nil)))
+
+
+
+  (add-hook 'post-command-hook 'org-latex-fragment-toggle-auto)
+  (setq org-latex-fragment-toggle-helper (byte-compile 'org-latex-fragment-toggle-helper))
+  (setq org-latex-fragment-toggle-auto (byte-compile 'org-latex-fragment-toggle-auto))
+  ;; -----
+
+
   (defvar org-captures-path (concat org-directory "captures.org"))
   (defvar org-todos-path (concat org-directory "todos.org"))
   (defvar org-school-path (concat org-directory "skola.org"))
@@ -81,7 +172,7 @@
        "* NOTE %?\n%U" :empty-lines 1)
       ("l" "Läxa/prov" entry (file+headline org-school-path "Prov _o_ sånt")
        "* TODO %^{Beskrivning}\n DEADLINE: %^t" :empty-lines 1)))
-  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
+  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.3))
   (setq org-hide-emphasis-markers t)
   (setq org-agenda-files (list org-directory))
   (setq org-log-done t)
@@ -269,6 +360,7 @@
   (defun configure-org-mode ()
     (sp-with-modes 'org-mode
       (sp-local-pair "\\(" "\\)")
+      (sp-local-pair "\\[" "\\]")
       (sp-local-pair "$" "$"))
     (sp-local-pair 'org-mode "*" "*" :actions :rem)
     (sp-local-pair 'org-mode "*" nil :actions :rem))
