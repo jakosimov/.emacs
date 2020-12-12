14e593bbe1e56af1a52cfae005c607f6571ab5f6
jakosimov <jakobalwall@gmail.com>
2020-12-12 20:23:53 +0100

    Fixa dante.

--- init.el
+++ init.el
@@ -45,6 +45,9 @@
       (set-face-attribute 'default nil :height 102)
     (set-face-attribute 'default nil :height 100)))
 
+(use-package lcr
+  :ensure t)
+
 (use-package org
   :ensure t
   :bind (("C-c c" . org-capture)
@@ -178,7 +181,7 @@
 (use-package doom-themes
   :ensure t
   :config
-  (defvar dark-theme 'doom-dracula) ;; doom-dracula, doom-gruvbox, doom-monokai-classic
+  (defvar dark-theme 'doom-gruvbox) ;; doom-dracula, doom-gruvbox, doom-monokai-classic
   (defvar light-theme 'doom-one-light)  ;; doom-one-light
   (defvar preferred-theme dark-theme)
   (defun switch-theme (theme)
@@ -325,7 +328,7 @@
 
 (use-package lsp-mode
   :ensure t
-  :hook ((typescript-mode c++-mode haskell-mode python-mode c-mode) . lsp)
+  :hook ((typescript-mode c++-mode python-mode c-mode) . lsp)
   :bind (:map lsp-mode-map
               ("C-c d" . lsp-find-definition)
               ("C-c r" . lsp-ui-peek-find-references)
@@ -350,12 +353,31 @@
       (defvar lsp-pyls-plugins-pycodestyle-ignore strict-python-warnings)
     (defvar lsp-pyls-plugins-pycodestyle-ignore incorrect-python-warnings)))
 
+(use-package lsp-haskell
+  :ensure t)
+
 (use-package company-lsp
   :ensure t
   :config
   (defvar company-lsp-enable-snippet nil))
 
-(use-package lsp-haskell
+(use-package haskell-mode
+  :ensure t)
+
+(use-package dante
+  :ensure t
+  :after haskell-mode
+  :commands 'dante-mode
+  :bind (:map haskell-mode-map
+              ("C-c d" . xref-find-definitions))
+  :init
+  (add-hook 'haskell-mode-hook 'flycheck-mode)
+  (put 'dante-repl-command-line 'safe-local-variable (lambda (_) t))
+  (put 'haskell-process-type 'safe-local-variable (lambda (_) t))
+
+  (add-hook 'haskell-mode-hook 'dante-mode))
+
+(use-package direnv
   :ensure t)
 
 (use-package lsp-ui
@@ -430,7 +452,7 @@
  '(custom-safe-themes
    '("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "cae81b048b8bccb7308cdcb4a91e085b3c959401e74a0f125e7c5b173b916bf9" "8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" "d5a878172795c45441efcd84b20a14f553e7e96366a163f742b95d65a3f55d71" "37a4701758378c93159ad6c7aceb19fd6fb523e044efe47f2116bc7398ce20c9" "4f01c1df1d203787560a67c1b295423174fd49934deb5e6789abd1e61dba9552" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" "9b272154fb77a926f52f2756ed5872877ad8d73d018a426d44c6083d1ed972b1" "bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "1f4b51dcecc5bdd2d4dc462a185de4d9e7845ccfbcbbf30a9fb3952e84f9e876" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" default))
  '(package-selected-packages
-   '(key-chord dashboard which-key evil web-mode company-ctags imenu-anywhere direnv yaml-mode rust-mode smartparens lsp-treemacs lsp-ui lsp-haskell company-lsp yasnippet org-bullets doom-modeline company flycheck lsp-ivy ivy-posframe ivy magit doom-themes vterm use-package)))
+   '(dante key-chord dashboard which-key evil web-mode company-ctags imenu-anywhere direnv yaml-mode rust-mode smartparens lsp-treemacs lsp-ui lsp-haskell company-lsp yasnippet org-bullets doom-modeline company flycheck lsp-ivy ivy-posframe ivy magit doom-themes vterm use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
